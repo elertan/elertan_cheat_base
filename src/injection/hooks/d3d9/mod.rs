777c367f7534ba2b12a873c41885b0e0d3b1947f
Dennis Kievits <denkievits@gmail.com>
2020-09-18 17:49:57 +0200

    Progress

--- src/injection/hooks/d3d9/mod.rs
+++ src/injection/hooks/d3d9/mod.rs
@@ -31,11 +31,14 @@ pub enum D3D9HookUninstallError {
 }
 
 type Direct3D9Create9Fn = unsafe extern "system" fn(version: UINT) -> *mut IDirect3D9;
-type EndSceneFn = unsafe extern "system" fn() -> HRESULT;
+type FnEndScene = unsafe extern "system" fn() -> HRESULT;
 
-#[no_mangle]
-extern "system" fn end_scene_hook() -> HRESULT {
-    0
+detour::static_detour! {
+    static EndSceneHook: unsafe extern "system" fn() -> HRESULT;
+}
+
+extern "system" fn end_scene_detour() -> HRESULT {
+    unsafe { EndSceneHook.call() }
 }
 
 #[no_mangle]
@@ -70,151 +73,102 @@ impl Hook<D3D9HookInstallError, D3D9HookUninstallError> for D3D9Hook {
 
         let module_name =
             CString::new(MODULE_NAME).expect("Could not turn MODULE_NAME into a C string");
-        let module = unsafe { GetModuleHandleA(module_name.as_ptr()) } as *mut HINSTANCE__;
+        let module = unsafe { GetModuleHandleA(module_name.as_ptr()) };
         if module == std::ptr::null_mut() {
             return Err(InstallError::Custom(D3D9HookInstallError::ModuleNotFound));
         }
         println!("Module handle d3d9.dll addr: {:p}", module);
 
-        let d3d9_device = unsafe {
-            let x = CString::new("Direct3DCreate9").unwrap();
-            let direct3d_create_9_addr =
-                std::mem::transmute::<_, Direct3D9Create9Fn>(GetProcAddress(module, x.as_ptr()));
-            let d3d9_ctx = (direct3d_create_9_addr)(D3D_SDK_VERSION);
-            println!("Created d3d9 ctx: {:?}", d3d9_ctx);
-            let mut display_mode: D3DDISPLAYMODE = std::mem::zeroed();
-            let d3d9_ctx_ref = d3d9_ctx.as_ref().unwrap();
-            d3d9_ctx_ref.GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &mut display_mode);
-            println!("Filled display_mode: {}", display_mode.Width);
-            let mut present_parameters: D3DPRESENT_PARAMETERS = std::mem::zeroed();
-            present_parameters.Windowed = 1;
-            present_parameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
-            present_parameters.BackBufferFormat = unsafe { display_mode.Format };
-            println!("Got present_parameters");
-
-            let wc_classname = CString::new("1").expect("Failed");
-            let mut wc: WNDCLASSEXA = WNDCLASSEXA {
-                cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
-                style: CS_CLASSDC,
-                lpfnWndProc: Some(tmp_wnd_proc),
-                cbClsExtra: 0,
-                cbWndExtra: 0,
-                hInstance: GetModuleHandleA(std::ptr::null()),
-                hIcon: std::ptr::null::<()>() as *mut HICON__,
-                hCursor: std::ptr::null::<()>() as *mut HICON__,
-                hbrBackground: std::ptr::null::<()>() as *mut HBRUSH__,
-                lpszMenuName: std::ptr::null::<()>() as *mut i8,
-                lpszClassName: wc_classname.as_ptr(),
-                hIconSm: std::ptr::null::<()>() as *mut HICON__,
-            };
-            RegisterClassExA(&mut wc);
-            println!("Registered class");
-            let hwnd = CreateWindowExA(
-                0,
-                wc_classname.as_ptr(),
-                std::ptr::null::<i8>(),
-                WS_EX_OVERLAPPEDWINDOW,
-                100,
-                100,
-                300,
-                300,
-                GetDesktopWindow(),
-                std::ptr::null::<()>() as *mut HMENU__,
-                wc.hInstance,
-                std::ptr::null::<()>() as *mut c_void,
-            );
-            println!("Created window: {}", std::mem::transmute::<_, u32>(hwnd));
-
-            let mut device: *mut IDirect3DDevice9 = std::ptr::null_mut();
-            let h_result = d3d9_ctx_ref.CreateDevice(
-                D3DADAPTER_DEFAULT,
-                D3DDEVTYPE_HAL,
-                hwnd,
-                D3DCREATE_SOFTWARE_VERTEXPROCESSING | D3DCREATE_DISABLE_DRIVER_MANAGEMENT,
-                &mut present_parameters,
-                &mut device,
-            );
-            let device_ref = device.as_ref().unwrap();
-            println!("HRESULT -> {}", h_result);
-            println!("Got d3d9 device -> {:?}", device);
-
-            let end_scene_addr = IDirect3DDevice9::EndScene as *const ();
-            println!("EndScene -> {:?}", end_scene_addr);
-
-            device
+        let vmt_pattern_scan_addr = unsafe {
+            pattern_scan(
+                module as *const u8,
+                0x128000,
+                &[
+                    Some(0xC7),
+                    Some(0x06),
+                    None,
+                    None,
+                    None,
+                    None,
+                    Some(0x89),
+                    Some(0x86),
+                    None,
+                    None,
+                    None,
+                    None,
+                    Some(0x89),
+                    Some(0x86),
+                ],
+            )
+            .expect("Pattern scan for d3d9 vmt failed")
         };
+        dbg!(vmt_pattern_scan_addr);
+        let vmt_pattern_scan_vmt_addr_offset = unsafe { vmt_pattern_scan_addr.offset(2) };
+        dbg!(vmt_pattern_scan_vmt_addr_offset);
+        let vmt_addr = unsafe { *(vmt_pattern_scan_vmt_addr_offset as *const u32) as *const u8 };
+        dbg!(vmt_addr);
 
-        // let vmt_addr = unsafe {
-        //     pattern_scan(
-        //         module,
-        //         0x128000,
-        //         &[
-        //             Some(0xC7),
-        //             Some(0x06),
-        //             None,
-        //             None,
-        //             None,
-        //             None,
-        //             Some(0x89),
-        //             Some(0x86),
-        //             None,
-        //             None,
-        //             None,
-        //             None,
-        //             Some(0x89),
-        //             Some(0x86),
-        //         ],
-        //     )
-        //     .expect("Pattern scan for d3d9 vmt failed")
-        //     .offset(2)
-        // };
+        let end_scene_idx = VmtMethods::EndScene as u8;
+        dbg!(end_scene_idx);
+        let end_scene_addr = unsafe { vmt_addr.offset(isize::from(end_scene_idx)) as *const u32 };
+        dbg!(end_scene_addr);
+        let end_scene: FnEndScene = unsafe { std::mem::transmute(end_scene_addr) };
 
-        // EndScene
-        {
-            let end_scene_addr = unsafe { std::ptr::null::<()>() as *mut c_void };
-            // let end_scene_addr = unsafe { vmt_addr.offset(42) };
-            // let end_scene = unsafe { std::mem::transmute::<_, EndSceneFn>(end_scene_addr) };
-            // println!("End Scene address: {:p}", end_scene_addr);
-
-            let mut old_protection: DWORD = 0;
-            // Get access to write vmt memory area
-            {
-                let success = unsafe {
-                    VirtualProtect(
-                        end_scene_addr as *mut c_void,
-                        4,
-                        PAGE_EXECUTE_READWRITE,
-                        &mut old_protection,
-                    )
-                };
-                if success == 0 {
-                    return Err(InstallError::Custom(
-                        D3D9HookInstallError::VirtualProtectVMT,
-                    ));
-                }
-            }
-
-            unsafe {
-                *(end_scene_addr as *mut u32) = std::mem::transmute::<_, u32>(&end_scene_hook);
-            }
-
-            // Restore previous protection for vmt memory area
-            {
-                let success = unsafe {
-                    VirtualProtect(
-                        end_scene_addr as *mut c_void,
-                        4,
-                        old_protection,
-                        &mut old_protection,
-                    )
-                };
-                if success == 0 {
-                    return Err(InstallError::Custom(
-                        D3D9HookInstallError::VirtualProtectVMTRestore,
-                    ));
-                }
-            }
-        }
+        let mut end_scene_hook = EndSceneHook
+            .initialize(end_scene, end_scene_detour)
+            .expect("Couldn't initialize EndScene hook");
+        end_scene_hook
+            .enable()
+            .expect("Couldn't enable EndScene hook");
+        // let vmt = unsafe { std::mem::transmute::<_, &[u32; 120]>(*vmt_pattern_scan_addr) };
+        // let end_scene_idx = VmtMethods::EndScene as usize;
+        // dbg!(end_scene_idx);
+        //
+        // // EndScene
+        // {
+        //     let end_scene_addr = unsafe { vmt[end_scene_idx] };
+        //     let end_scene = unsafe { std::mem::transmute::<_, EndSceneFn>(end_scene_addr) };
+        //     println!("End Scene address: {:?}", end_scene_addr as *mut ());
+        //
+        //     let mut old_protection: DWORD = 0;
+        //     // Get access to write vmt memory area
+        //     {
+        //         let success = unsafe {
+        //             VirtualProtect(
+        //                 end_scene_addr as *mut c_void,
+        //                 4,
+        //                 PAGE_EXECUTE_READWRITE,
+        //                 &mut old_protection,
+        //             )
+        //         };
+        //         if success == 0 {
+        //             return Err(InstallError::Custom(
+        //                 D3D9HookInstallError::VirtualProtectVMT,
+        //             ));
+        //         }
+        //     }
+        //
+        //     unsafe {
+        //         *(end_scene_addr as *mut u32) = std::mem::transmute::<_, u32>(&end_scene_hook);
+        //     }
+        //
+        //     // Restore previous protection for vmt memory area
+        //     {
+        //         let success = unsafe {
+        //             VirtualProtect(
+        //                 end_scene_addr as *mut c_void,
+        //                 4,
+        //                 old_protection,
+        //                 &mut old_protection,
+        //             )
+        //         };
+        //         if success == 0 {
+        //             return Err(InstallError::Custom(
+        //                 D3D9HookInstallError::VirtualProtectVMTRestore,
+        //             ));
+        //         }
+        //     }
+        // }
 
         self.installed = true;
         Ok(())
@@ -236,4 +190,128 @@ impl Drop for D3D9Hook {
         }
         self.uninstall().expect("Could not uninstall");
     }
+}
+
+#[repr(u8)]
+#[allow(unused)]
+enum VmtMethods {
+    QueryInterface,
+    AddRef,
+    Release,
+    TestCooperativeLevel,
+    GetAvailableTextureMem,
+    EvictManagedResources,
+    GetDirect3D,
+    GetDeviceCaps,
+    GetDisplayMode,
+    GetCreationParameters,
+    SetCursorProperties,
+    SetCursorPosition,
+    ShowCursor,
+    CreateAdditionalSwapChain,
+    GetSwapChain,
+    GetNumberOfSwapChains,
+    Reset,
+    Present,
+    GetBackBuffer,
+    GetRasterStatus,
+    SetDialogBoxMode,
+    SetGammaRamp,
+    GetGammaRamp,
+    CreateTexture,
+    CreateVolumeTexture,
+    CreateCubeTexture,
+    CreateVertexBuffer,
+    CreateIndexBuffer,
+    CreateRenderTarget,
+    CreateDepthStencilSurface,
+    UpdateSurface,
+    UpdateTexture,
+    GetRenderTargetData,
+    GetFrontBufferData,
+    StretchRect,
+    ColorFill,
+    CreateOffscreenPlainSurface,
+    SetRenderTarget,
+    GetRenderTarget,
+    SetDepthStencilSurface,
+    GetDepthStencilSurface,
+    BeginScene,
+    EndScene,
+    Clear,
+    SetTransform,
+    GetTransform,
+    MultiplyTransform,
+    SetViewport,
+    GetViewport,
+    SetMaterial,
+    GetMaterial,
+    SetLight,
+    GetLight,
+    LightEnable,
+    GetLightEnable,
+    SetClipPlane,
+    GetClipPlane,
+    SetRenderState,
+    GetRenderState,
+    CreateStateBlock,
+    BeginStateBlock,
+    EndStateBlock,
+    SetClipStatus,
+    GetClipStatus,
+    GetTexture,
+    SetTexture,
+    GetTextureStageState,
+    SetTextureStageState,
+    GetSamplerState,
+    SetSamplerState,
+    ValidateDevice,
+    SetPaletteEntries,
+    GetPaletteEntries,
+    SetCurrentTexturePalette,
+    GetCurrentTexturePalette,
+    SetScissorRect,
+    GetScissorRect,
+    SetSoftwareVertexProcessing,
+    GetSoftwareVertexProcessing,
+    SetNPatchMode,
+    GetNPatchMode,
+    DrawPrimitive,
+    DrawIndexedPrimitive,
+    DrawPrimitiveUP,
+    DrawIndexedPrimitiveUP,
+    ProcessVertices,
+    CreateVertexDeclaration,
+    SetVertexDeclaration,
+    GetVertexDeclaration,
+    SetFVF,
+    GetFVF,
+    CreateVertexShader,
+    SetVertexShader,
+    GetVertexShader,
+    SetVertexShaderConstantF,
+    GetVertexShaderConstantF,
+    SetVertexShaderConstantI,
+    GetVertexShaderConstantI,
+    SetVertexShaderConstantB,
+    GetVertexShaderConstantB,
+    SetStreamSource,
+    GetStreamSource,
+    SetStreamSourceFreq,
+    GetStreamSourceFreq,
+    SetIndices,
+    GetIndices,
+    CreatePixelShader,
+    SetPixelShader,
+    GetPixelShader,
+    SetPixelShaderConstantF,
+    GetPixelShaderConstantF,
+    SetPixelShaderConstantI,
+    GetPixelShaderConstantI,
+    SetPixelShaderConstantB,
+    GetPixelShaderConstantB,
+    DrawRectPatch,
+    DrawTriPatch,
+    DeletePatch,
+    CreateQuery,
 }
